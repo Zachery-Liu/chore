371acc3e839bfab23ed7d699e48a6b46680cf937
Zachery-Liu <61861732+Zachery-Liu@users.noreply.github.com>
2022-02-12 15:57:30 +0800

    Update click.js

--- click.js
+++ click.js
@@ -1,141 +1,43 @@
-/*!
- * Fairy Dust Cursor.js
- * - 90's cursors collection
- * -- https://github.com/tholman/90s-cursor-effects
- * -- http://codepen.io/tholman/full/jWmZxZ/
- */
+        (function () {
+            var a_idx = 0;
+            window.onclick = function (event) {
+                var a = new Array("❤富强❤", "❤民主❤", "❤文明❤", "❤和谐❤", "❤自由❤", "❤平等❤", "❤公正❤", "❤法治❤", "❤爱国❤",
+                    "❤敬业❤", "❤诚信❤", "❤友善❤");
 
-(function fairyDustCursor() {
-  
-  var possibleColors = ["#D61C59", "#E7D84B", "#1B8798"]
-  var width = window.innerWidth;
-  var height = window.innerHeight;
-  var cursor = {x: width/2, y: width/2};
-  var particles = [];
-  
-  function init() {
-    bindEvents();
-    loop();
-  }
-  
-  // Bind events that are needed
-  function bindEvents() {
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('touchmove', onTouchMove);
-    document.addEventListener('touchstart', onTouchMove);
-    
-    window.addEventListener('resize', onWindowResize);
-  }
-  
-  function onWindowResize(e) {
-    width = window.innerWidth;
-    height = window.innerHeight;
-  }
-  
-  function onTouchMove(e) {
-    if( e.touches.length > 0 ) {
-      for( var i = 0; i < e.touches.length; i++ ) {
-        addParticle( e.touches[i].clientX, e.touches[i].clientY, possibleColors[Math.floor(Math.random()*possibleColors.length)]);
-      }
-    }
-  }
-  
-  function onMouseMove(e) {    
-    cursor.x = e.clientX;
-    cursor.y = e.clientY;
-    
-    addParticle( cursor.x, cursor.y, possibleColors[Math.floor(Math.random()*possibleColors.length)]);
-  }
-  
-  function addParticle(x, y, color) {
-    var particle = new Particle();
-    particle.init(x, y, color);
-    particles.push(particle);
-  }
-  
-  function updateParticles() {
-    
-    // Updated
-    for( var i = 0; i < particles.length; i++ ) {
-      particles[i].update();
-    }
-    
-    // Remove dead particles
-    for( var i = particles.length -1; i >= 0; i-- ) {
-      if( particles[i].lifeSpan < 0 ) {
-        particles[i].die();
-        particles.splice(i, 1);
-      }
-    }
-    
-  }
-  
-  function loop() {
-    requestAnimationFrame(loop);
-    updateParticles();
-  }
-  
-  /**
-   * Particles
-   */
-  
-  function Particle() {
+                var heart = document.createElement("b"); //创建b元素
+                heart.onselectstart = new Function('event.returnValue=false'); //防止拖动
 
-    this.character = "*";
-    this.lifeSpan = 120; //ms
-    this.initialStyles ={
-      "position": "absolute",
-      "display": "block",
-      "pointerEvents": "none",
-      "z-index": "10000000",
-      "fontSize": "16px",
-      "will-change": "transform"
-    };
+                document.body.appendChild(heart).innerHTML = a[a_idx]; //将b元素添加到页面上
+                a_idx = (a_idx + 1) % a.length;
+                heart.style.cssText = "position: fixed;left:-100%;"; //给p元素设置样式
 
-    // Init, and set properties
-    this.init = function(x, y, color) {
+                var f = 16, // 字体大小
+                    x = event.clientX - f / 2, // 横坐标
+                    y = event.clientY - f, // 纵坐标
+                    c = randomColor(), // 随机颜色
+                    a = 1, // 透明度
+                    s = 1.2; // 放大缩小
 
-      this.velocity = {
-        x:  (Math.random() < 0.5 ? -1 : 1) * (Math.random() / 2),
-        y: 1
-      };
-      
-      this.position = {x: x - 10, y: y - 20};
-      this.initialStyles.color = color;
-      console.log(color);
+                var timer = setInterval(function () { //添加定时器
+                    if (a <= 0) {
+                        document.body.removeChild(heart);
+                        clearInterval(timer);
+                    } else {
+                        heart.style.cssText = "font-size:16px;cursor: default;position: fixed;color:" +
+                            c + ";left:" + x + "px;top:" + y + "px;opacity:" + a + ";transform:scale(" +
+                            s + ");";
 
-      this.element = document.createElement('span');
-      this.element.innerHTML = this.character;
-      applyProperties(this.element, this.initialStyles);
-      this.update();
-      
-      document.body.appendChild(this.element);
-    };
-    
-    this.update = function() {
-      this.position.x += this.velocity.x;
-      this.position.y += this.velocity.y;
-      this.lifeSpan--;
-      
-      this.element.style.transform = "translate3d(" + this.position.x + "px," + this.position.y + "px,0) scale(" + (this.lifeSpan / 120) + ")";
-    }
-    
-    this.die = function() {
-      this.element.parentNode.removeChild(this.element);
-    }
-    
-  }
-  
-  /**
-   * Utils
-   */
-  
-  // Applies css `properties` to an element.
-  function applyProperties( target, properties ) {
-    for( var key in properties ) {
-      target.style[ key ] = properties[ key ];
-    }
-  }
-  
-  init();
-})();
+                        y--;
+                        a -= 0.016;
+                        s += 0.002;
+                    }
+                }, 15)
+
+            }
+            // 随机颜色
+            function randomColor() {
+
+                return "rgb(" + (~~(Math.random() * 255)) + "," + (~~(Math.random() * 255)) + "," + (~~(Math
+                .random() * 255)) + ")";
+            }
+        }());
